83583690364784bcd27380ce2d6c6fe3a46ebb0f
Pedro Antônio <pedroantonioytbr@gmail.com>
2025-10-08 09:44:21 -0300

    commit2

--- src/pages/Love.jsx
+++ src/pages/Love.jsx
@@ -1,8 +1,92 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import styles from "./Love.module.css"
 
 const Love = () => {
+
+  const inicio = new Date("2024-11-03")
+
+  const [tempoJuntos, setTempoJuntos] = useState({
+    anos: 0,
+    meses: 0,
+    dias: 0,
+    horas: 0,
+    minutos: 0,
+    segundos: 0
+  })
+
+
+  const calcularTempoJuntos = () => {
+    const agora = new Date()
+
+    let anos = agora.getFullYear() - inicio.getFullYear()
+    let meses = agora.getMonth() - inicio.getMonth()
+    let dias = agora.getDate() - inicio.getDate()
+    let horas = agora.getHours() - inicio.getHours()
+    let minutos = agora.getMinutes() - inicio.getMinutes()
+    let segundos = agora.getSeconds() - inicio.getSeconds()
+
+    // Ajustes de negativos
+    if (segundos < 0) {
+      segundos += 60
+      minutos--
+    }
+
+    if (minutos < 0) {
+      minutos += 60
+      horas--
+    }
+
+    if (horas < 0) {
+      horas += 24
+      dias--
+    }
+
+    if (dias < 0) {
+      meses--
+      const ultimoDiaMesAnterior = new Date(agora.getFullYear(), agora.getMonth(), 0)
+      dias += ultimoDiaMesAnterior.getDate()
+    }
+
+    if (meses < 0) {
+      anos--
+      meses += 12
+    }
+
+    setTempoJuntos({ anos, meses, dias, horas, minutos, segundos })
+  }
+
+  useEffect(() => {
+    calcularTempoJuntos()
+    const timer = setInterval(calcularTempoJuntos, 1000) // atualiza a cada segundo
+    return () => clearInterval(timer)
+  }, [])
+
   return (
-    <div>Love</div>
+    <nav className={styles.shapeCard} >
+      <h1>Nosso Amor</h1>
+      <ul className={styles.display}>
+        <ul className={styles.timer}>
+          <li>Anos: {tempoJuntos.anos} </li>
+          <li>Meses: {tempoJuntos.meses} </li>
+          <li>Dias: {tempoJuntos.dias} </li>
+        </ul>
+        <ul className={styles.timer}>
+          <li>Horas: {tempoJuntos.horas} </li>
+          <li>Minutos: {tempoJuntos.minutos} </li>
+          <li>Segundos: {tempoJuntos.segundos} </li>
+        </ul>
+      </ul>
+      <div className={styles.firstPhoto}>
+        <h1>08/12/2024</h1>
+        <img src="/public/casamento.jpeg" className={styles.casamento} />
+        <p>Nesse dia postamos nossa primeira foto juntos no insta, nos assumindo para o mundo, ainda receosos sobre o futuro, mas graças a Deus tudo deu certo</p>
+      </div>
+      <div className={styles.rioPhoto}>
+        <h1>29/12/2024</h1>
+        <img src="/public/rio.jpeg" className={styles.rio} />
+        <p>Por muito tempo essa foi minha foto favorita nossa, foi a primeira foto que a gente pareceu mais casados que namorados</p>
+      </div>
+    </nav>
   )
 }
 
